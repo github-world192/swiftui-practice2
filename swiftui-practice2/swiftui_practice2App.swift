0b5fc544f68e1b9e1bdd24a3c9eefbcccd89ed7c
github-world192 <dillen99999999@gmail.com>
2023-09-25 18:32:09 -0700

    Initial Commit

---
+++ swiftui-practice2/swiftui_practice2App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_practice2App.swift
+//  swiftui-practice2
+//
+//  Created by llll on 2023/9/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_practice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
